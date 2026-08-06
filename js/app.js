4220906b94f8163186f19dfe9ede474eeb9db6e3
easycode-sniper <ferdjellahsouhaibomd@gmail.com>
2026-08-06 21:56:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+alert("MODULE LOADED!");
 // --- FIREBASE CONFIGURATION START ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
